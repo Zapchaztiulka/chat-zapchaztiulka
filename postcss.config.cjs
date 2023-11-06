028837838e3756af304ba8b1bf5d73a92c4188fe
Oleh Kliapko <goldclub1977@gmail.com>
2023-11-06 18:10:26 +0200

    FIX: tailwind nected config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,8 @@
 export default {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss/nesting': {},
     tailwindcss: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': false },
-    },
+    autoprefixer: {},
   },
 };
