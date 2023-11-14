c8fbc25133d4c8fed2337c7178196e183f5589e3
Oleh Kliapko <goldclub1977@gmail.com>
2023-11-14 16:03:49 +0200

    FIX: update to universal buttons in another components

--- src/pages/FAQPage/FAQPage.jsx
+++ src/pages/FAQPage/FAQPage.jsx
@@ -1,10 +1,7 @@
-import { SecondaryBtn } from '../../components/Button';
-
 export const FAQPage = () => {
   return (
     <>
       <div>FAQPage</div>
-      <SecondaryBtn to="/">Повернутися до меню</SecondaryBtn>
     </>
   );
 };
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'universal-components-frontend/src/components';
+
 import './styles.css';
-import { PrimaryBtn } from '@/components/Button';
 import errorPage from '@/images/404-page.gif';
 
 export const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="wrapper">
       <img className="image" src={errorPage} alt="" />
       <div className="title">Oops! Page not found</div>
-      <PrimaryBtn to="/">Go back to chat</PrimaryBtn>
+      <Button text="Go back to chat" onClick={() => navigate('/')} />
     </div>
   );
 };
