1afe462b3cc04b35172fd140468689e32344d8b9
Oleh Kliapko <goldclub1977@gmail.com>
2023-11-25 11:18:28 +0200

    Updates

--- assets/index-512b3abd.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-6ce9ac37.js";const t=()=>e.jsx("div",{children:"OrderDetailsPage"});export{t as default};
--- assets/index-a3100f4c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-6ce9ac37.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("div",{children:"FAQPage"})});export{t as default};
